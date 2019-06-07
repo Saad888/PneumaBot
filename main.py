ab30716c4a64b1e48971aad01643eb0db714a1a0
Xaad <Saad888@users.noreply.github.com>
2019-06-06 23:10:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import json
 import asyncio
+import re
 
 class PneumaClient(discord.Client):
 
@@ -11,6 +12,7 @@ class PneumaClient(discord.Client):
         self.admin_chan = None
         self.command_chan = None
         self.core_message = None
+        self.emoji_to_channel = {}
         loop = asyncio.get_event_loop()
         loop.create_task(self.start(self.token))
         try:
@@ -19,37 +21,120 @@ class PneumaClient(discord.Client):
             loop.stop()
             print('Bot has Disconnected')
 
+    async def update_data(self):
+        self.emoji_update()
+        await self.update_core()
+        msg = '<@!93172449562066944> update the configs on heroku'
+        await self.admin_chan.send(msg)
+
+
     async def on_message(self, message):
         if message.author == self.user:
             return
 
-
         if message.channel.id == self.configs["Admin Channel"]:
+
+            if message.content.startswith('!add'):
+                msg = message.content
+                channel_block = re.search(r'!add [a-zA-Z0-9]+', msg)
+                role_block = re.search(r'<@&[0-9]+>', msg)
+                emoji_block = re.search(r'<:[a-zA-Z0-9_]+:[0-9]+>', msg)
+                if channel_block is None: 
+                    await self.admin_chan.send('Cant find name')
+                    return
+                if role_block is None:
+                    await self.admin_chan.send('Cant find role')
+                    return
+                if emoji_block is None:
+                    await self.admin_chan.send('Cant find Emoji')
+                    return
+                channel = channel_block.group().replace('!add ', '')
+                role_id = re.search('[0-9]+', role_block.group()).group()
+                emoji_id = re.search(':[0-9]+>', emoji_block.group()).group()
+                emoji_id = emoji_id.replace(':', '').replace('>', '')
+
+                try:
+                    await message.guild.fetch_emoji(emoji_id)
+                except (discord.errors.NotFound):
+                    await self.admin_chan.send('Emoji not found on this server')
+                    return
+
+
+                msg = 'Add Channel {0} for role {1} using emoji {2}?'.format(
+                    channel_block.group(), 
+                    role_block.group(),
+                    emoji_block.group()
+                )
+                msg += '\nType !YES to continue...'
+                await self.admin_chan.send(msg)
+
+                def check(msg):
+                    return msg.author.id == message.author.id
+                ret = await self.wait_for('message', check=check, timeout=30)
+
+                if ret.content == '!YES':
+                    IDs = [int(role_id), int(emoji_id)]
+                    self.configs['Channels'][channel] = IDs
+                    await self.admin_chan.send('Added Role')
+                    await self.update_data()
+                else:
+                    await self.admin_chan.send('Doing nothing')
+
+                
             if message.content.startswith('!update'):
                 await self.update_core()
 
             if message.content.startswith('!configs'):
-                configs_string = json.dumps(self.configs, indent=4, sort_keys=True)
-                if len(configs_string) > 2000:
-                    for i in range(len(configs_string) // 2000):
+                cs = json.dumps(self.configs, indent=4, sort_keys=True)
+                if len(cs) > 2000:
+                    for i in range(len(cs) // 2000):
                         await self.admin_chan.send(
-                            configs_string[i * 2000: min(
+                            cs[i * 2000: min(
                                 i * 2000 + 1999, 
-                                len(configs_string))])
+                                len(cs))])
                 else:
-                    await self.admin_chan.send(configs_string)
+                    await self.admin_chan.send(cs)
                 
-            if message.content.startswith('!clear'):
-                await self.admin_chan.purge()
-
             if message.content.startswith('!test'):
-                await self.admin_chan.send('Sending message to admins <:rayeban:575175949356892192>')
+                await self.admin_chan.send('test message')
 
             if message.content.startswith('!exit'):
                 await message.channel.send('Leaving')
                 await self.logout()
                 loop = asyncio.get_running_loop()
                 loop.stop()
+
+
+    async def on_raw_reaction_add(self, payload):
+        result = self.role_from_react(payload)
+        if result is not None:
+            role, user = result
+            await user.add_roles(role)
+
+
+    async def on_raw_reaction_remove(self, payload):
+        result = self.role_from_react(payload)
+        if result is not None:
+            role, user = result
+            await user.remove_roles(role)
+
+
+    def role_from_react(self, payload):
+        user_ID = payload.user_id
+        message = payload.message_id
+        emoji_ID = payload.emoji.id
+
+        if user_ID == self.user.id:
+            return
+
+        if message == self.core_message.id:
+            if emoji_ID in self.emoji_to_channel:
+                chan_name = self.emoji_to_channel[emoji_ID]
+                role_ID = self.configs['Channels'][chan_name][0]
+                guild = self.get_emoji(emoji_ID).guild
+                role = guild.get_role(role_ID)
+                member = guild.get_member(user_ID)
+                return role, member
 
 
     async def find_core(self):
@@ -67,42 +152,77 @@ class PneumaClient(discord.Client):
             # Saves core message
             self.core_message = msg
 
+            # Update core
+            await self.update_core()
+
         except discord.NotFound:
             await self.start_core()
+            await self.find_core()
 
 
     async def start_core(self):
         # Sends the core message and assigns it on internal values
         core = await self.command_chan.send('IM')
         self.configs['Main Message ID'] = core.id
+        msg = '<@!93172449562066944> update the configs on heroku'
+        await self.admin_chan.send(msg)
         self.core_message = core
 
 
     async def update_core(self):
         msg = self.configs['MSGs']['Core Message']
+        embed = discord.Embed(
+            title='**Hello!**', 
+            description=msg, 
+            color=0x17f0d6
+        )
         emojis = []
+        embed_count = 0
         for chan, IDs in self.configs['Channels'].items():
             if chan != 'AAAAA':
-                emoji_ID = IDs[2]
+                emoji_ID = IDs[1]
                 target_emoji = self.get_emoji(emoji_ID)
                 emojis.append(target_emoji)
-                msg += f'<:{target_emoji.name}:{target_emoji.id}> - {chan}\n' 
-        await self.core_message.edit(content=msg)
+                embed.add_field(
+                    name=chan, 
+                    value=f'<:{target_emoji.name}:{target_emoji.id}>', 
+                    inline=True
+                )
+                embed_count += 1
+        
+        pad_count = (3 - embed_count % 3) % 3
+
+        for i in range(pad_count):
+            embed.add_field(name='-', value='-', inline=True)
+
+        embed.set_footer(text=self.configs['MSGs']['Core Footer'])
+        await self.core_message.edit(content='', embed=embed)
         for emoji in emojis:
             await self.core_message.add_reaction(emoji)
 
 
+    def emoji_update(self):
+        for chan, IDs in self.configs['Channels'].items():
+            self.emoji_to_channel[IDs[1]] = chan
 
 
     async def on_ready(self):
-        print('Ready!')
         self.admin_chan = self.get_channel(self.configs["Admin Channel"])
         self.command_chan = self.get_channel(self.configs["Command Channel"])
         await self.admin_chan.send('Loaded...')
         await self.find_core()
+        self.emoji_update()
+
+        print('Ready!')
         print(self.user.name)
         print(self.user.id)
         print('=' * 5)
+
+    async def on_error(self, event, *args, **kwargs):
+        await super().on_error(event, *args, **kwargs)
+        msg = '<@!93172449562066944> there was an error!'
+        await self.admin_chan.send(msg)
+
 
 
 configs = {}
